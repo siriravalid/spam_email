3113bb0c39ba6b61d9b2bc0640b15e440adfcdcc
siriravalid <152952587+siriravalid@users.noreply.github.com>
2024-08-09 18:37:17 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv('spam.csv', encoding='latin-1')
+df = df[['v1', 'v2']]  
+df.columns = ['Label', 'Email']
+df['Label'] = df['Label'].map({'spam': 'spam', 'ham': 'not spam'})
+X = df['Email']
+y = df['Label']
+vectorizer = TfidfVectorizer(stop_words='english')
+X = vectorizer.fit_transform(X) 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+model = MultinomialNB()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+st.title("Spam Email Classifier(Naive Bayes)- Siri ")
+st.write(f"Model Accuracy: {accuracy * 100:.2f}%")
+
+email_input = st.text_area("Enter the content for email:")
+if st.button("Classify"):
+    if email_input:
+        input_vector = vectorizer.transform([email_input])
+        prediction = model.predict(input_vector)[0]
+        st.write(f"The email is classified as: **{prediction}**")
+    else:
+        st.write("Please enter some email content.")
